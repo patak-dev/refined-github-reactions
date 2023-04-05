502e90cd243df9312ddb1dbad825b12489bacaf2
patak <matias.capeletto@gmail.com>
2023-04-05 17:08:17 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Refined GitHub Reactions 
+
+Enhances the GitHub Reactions page, making them more compact.
+
+[Install on Greasyfork](https://greasyfork.org)
+
+## Sponsors
+
+<p align="center">
+  <a href="https://cdn.jsdelivr.net/gh/patak-dev/static/sponsors.svg">
+    <img src='https://cdn.jsdelivr.net/gh/patak-dev/static/sponsors.svg'/>
+  </a>
+</p>
+
+## Credits
+
+Setup based on [@antfu Refined GitHub Reactions](https://github.com/antfu/refined-github-notifications).
+
+## License
+
+[MIT](./LICENSE) License © 2022 [Matias Capeletto](https://github.com/patak-dev)
---
+++ index.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         Refined Reactions
+// @license      MIT
+// @homepageURL  https://github.com/patak-dev/refined-github-reactions
+// @supportURL   https://github.com/patak-dev/refined-github-reactions
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Enhances the GitHub Reactions page, making them more compact.
+// @author       patak-dev
+// @match        https://github.com/**
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  const style = document.createElement('style')
+  style.innerHTML = `
+.js-reactions-container {
+flex-direction: row !important;
+position: absolute;
+right: 5px;
+transform: translate(0px, -13px);
+}
+.js-reactions-container .js-reaction-group-button {
+background-color: var(--color-canvas-subtle);
+}
+.discussion-timeline-actions {
+border-top: 0;
+}
+`
+  document.head.appendChild(style)
+})();
